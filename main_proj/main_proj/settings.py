3cee2915a3fa14d755d0bc6b0dbe342d562ae691
Artur <geraskov94@mail.ru>
2021-06-04 18:13:36 +0200

    Add new branch

--- main_proj/main_proj/settings.py
+++ main_proj/main_proj/settings.py
@@ -75,9 +75,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main_proj.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
