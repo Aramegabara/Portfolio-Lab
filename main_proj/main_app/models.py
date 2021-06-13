7b1acd07a8073a9c905dbcfaf819dc51c00ee337
Artur <geraskov94@mail.ru>
2021-06-13 15:01:00 +0200

    model Donation (import user_model)

--- main_proj/main_app/models.py
+++ main_proj/main_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
 class Category(models.Model):
@@ -23,3 +27,16 @@ class Institution(models.Model):
         choices=TYPE_INSTITUTION,
         default=V1
     )
+
+class Donation(models.Model):
+    quantity = models.PositiveIntegerField(default=1)
+    categories = models.ManyToManyField(Category, verbose_name='Categories')
+    institution  = models.ForeignKey('Institution', verbose_name='Institution', on_delete=models.CASCADE)
+    address = models.CharField(max_length=200, verbose_name='Address',)
+    phone_number = models.DecimalField(max_digits=9, decimal_places=0, verbose_name='Phone')
+    city = models.CharField(max_length=100, verbose_name='City')
+    zip_code = models.CharField( max_length=5, default="45000", verbose_name='Zip code')
+    pick_up_date = models.DateField(verbose_name='Date pick up', default=timezone.now)
+    pick_up_time = models.DateTimeField(auto_now=True, verbose_name='Time pick up')
+    pick_up_comment = models.TextField(verbose_name='Comment', null=True, blank=True)
+    user = models.ForeignKey(User, null=True, verbose_name='User', on_delete=models.CASCADE)
